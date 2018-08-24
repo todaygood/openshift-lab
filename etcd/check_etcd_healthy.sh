f9ff883cf40df1bbdc629def5994de1c0c90a229
Margin <hujun1@genomics.cn>
2018-08-24 15:26:05 +0800

    deploy ok

--- etcd/check_etcd_healthy.sh
+++ etcd/check_etcd_healthy.sh
@@ -1,8 +1,20 @@
+#!/bin/bash
 
-ENDPOINTS=http://ose0.cloud.genomics.cn:2379,http://ose1.cloud.genomics.cn:2379,http://ose2.cloud.genomics.cn:2379
+ENDPOINTS=https://ose0.cloud.genomics.cn:2379,https://ose1.cloud.genomics.cn:2379,https://ose2.cloud.genomics.cn:2379
 
-etcdctl -C $ENDPOINTS  \
+
+command=" etcdctl -C $ENDPOINTS  \
      --ca-file=/etc/origin/master/master.etcd-ca.crt \
      --cert-file=/etc/origin/master/master.etcd-client.crt \
-     --key-file=/etc/origin/master/master.etcd-client.key cluster-health
+     --key-file=/etc/origin/master/master.etcd-client.key "
+     
+echo "cluster health:"     
+$command  cluster-health
+
+echo "member list"
+$command member list
+
+
+echo "user list"
+$command user list
 
